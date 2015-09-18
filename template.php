ce5938cb6a0afbaf254b6373ad892320a284c9fe
Guillaume Viguier <guillaume@viguierjust.com>
2015-09-18 08:23:46 +0200

    Added fontawesome + footer config

--- template.php
+++ template.php
@@ -3,3 +3,14 @@
  * @file
  * template.php
  */
+
+function cqfd_preprocess_html(&$variables) {
+  drupal_add_css('https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css', array('type' => 'external'));
+}
+
+function cqfd_preprocess_region(&$variables, $hook) {
+  if($variables['region'] == "footer"){
+    $variables['classes_array'][] = 'container';
+  }
+}
+
