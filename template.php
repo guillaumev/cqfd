b6b3a78a31b41f1a2e64834b40908ca4fdc83f68
Guillaume Viguier <guillaume@viguierjust.com>
2015-10-06 15:23:38 +0200

    Added font

--- template.php
+++ template.php
@@ -6,6 +6,7 @@
 
 function cqfd_preprocess_html(&$variables) {
   drupal_add_css('https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css', array('type' => 'external'));
+  drupal_add_css('https://fonts.googleapis.com/css?family=Radley', array('type' => 'external'));
 }
 
 function cqfd_preprocess_region(&$variables, $hook) {
